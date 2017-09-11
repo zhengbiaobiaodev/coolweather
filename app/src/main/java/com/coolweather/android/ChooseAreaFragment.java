7d2b86929016f710f2f0ed11f5e77cfbb1034ca3
Jock <biaobiao.zheng@gmail.com>
2017-09-11 21:13:08 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -113,7 +113,6 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
                     WeatherActivity.setmWeatherId(weatherId);
-//                    WeatherActivity.mWeatherId = countyList.get(position).getWeatherId();
                     if (getActivity() instanceof MainActivity) {
                         Intent intent = new Intent(getActivity(), WeatherActivity.class);
                         intent.putExtra("weather_id", weatherId);
